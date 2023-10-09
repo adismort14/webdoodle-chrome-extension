8b0fff29b757641db40ba18083603a2f8189ec39
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-10-09 09:33:08 +0530

    debugged the is mouse pressed feature

--- scripts/content.js
+++ scripts/content.js
@@ -3,6 +3,7 @@ console.log("content script is running.");
 var s = p => {
    p.setup=function(){
         // console.log("this is setup");
+        document.body.style['userSelect']='none';
         let h=document.body.clientHeight;
         let c=p.createCanvas(p.windowWidth, h);
         c.position(0,0);
@@ -15,9 +16,9 @@ var s = p => {
         p.stroke(0);
         p.strokeWeight(4);
         // background(0);
-        // if(isMousePressed){
+        if(p.mouseIsPressed){
             p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-        // }
+        }
     }
 
   };
