5acb434e1ac4d9d101b13446135b3fa89d079568
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-10-10 10:28:27 +0530

    added different pen colors

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,4 @@
 // console.log("content script is running.");
-let cnt=0;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action==='generate') {
@@ -16,17 +15,87 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 p.draw=function(){
                     p.stroke(0);
                     p.strokeWeight(4);
-                    // background(0);
                     if(p.mouseIsPressed){
                         p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
                     }
                 }
             
             };
-    
-        new p5(s); 
+            new p5(s);
+        }
+
+        if(message.data==='Red'){
+            // console.log('Red');
+            var s = p => {
+                p.setup=function(){
+                        document.body.style['userSelect']='none';
+                        let h=document.body.clientHeight;
+                        let c=p.createCanvas(p.windowWidth, h);
+                        c.position(0,0);
+                        c.style('pointer-events','none');
+                        p.clear();
+                    }
+                    
+                    p.draw=function(){
+                        p.stroke(255,0,0);
+                        p.strokeWeight(4);
+                        if(p.mouseIsPressed){
+                            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+                        }
+                    }
+                
+                };
+                new p5(s);
+           
+        }
+        else if(message.data==='Blue'){
+            var s = p => {
+                p.setup=function(){
+                        document.body.style['userSelect']='none';
+                        let h=document.body.clientHeight;
+                        let c=p.createCanvas(p.windowWidth, h);
+                        c.position(0,0);
+                        c.style('pointer-events','none');
+                        p.clear();
+                    }
+                    
+                    p.draw=function(){
+                        p.stroke(0,0,255);
+                        p.strokeWeight(4);
+                        if(p.mouseIsPressed){
+                            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+                        }
+                    }
+                
+                };
+                new p5(s);
+            
+        }
+        else if(message.data==='Black'){
+            var s = p => {
+                p.setup=function(){
+                        document.body.style['userSelect']='none';
+                        let h=document.body.clientHeight;
+                        let c=p.createCanvas(p.windowWidth, h);
+                        c.position(0,0);
+                        c.style('pointer-events','none');
+                        p.clear();
+                    }
+                    
+                    p.draw=function(){
+                        p.stroke(0);
+                        p.strokeWeight(4);
+                        if(p.mouseIsPressed){
+                            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+                        }
+                    }
+                
+                };
+                new p5(s);
+            
+        }
     }
-  });
+  );
 
   
 
--- scripts/popup.js
+++ scripts/popup.js
@@ -9,12 +9,25 @@ var toggle=document.getElementById("toggleButton").addEventListener('click',func
         document.getElementById('toggleButton').innerHTML="Clicked";
         cnt++;
     }
-    else{
+    // else{
+    //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //         const activeTab = tabs[0];
+    //         chrome.tabs.sendMessage(activeTab.id, { action: 'clear' });
+    //     });
+    //     document.getElementById('toggleButton').innerHTML="Doodle Away";
+    //     cnt=0;
+    // } 
+},false);
+
+var penColors=document.getElementsByClassName("buttonStyle");
+
+for(const penColor of penColors){
+    penColor.addEventListener('click',function(){
+        // console.log(penColor.innerHTML);
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, { action: 'clear' });
+            chrome.tabs.sendMessage(activeTab.id, { data: penColor.innerHTML });
         });
-        document.getElementById('toggleButton').innerHTML="Doodle Away";
-        cnt=0;
-    } 
-},false);
+    })
+}
+
